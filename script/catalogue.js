6cad3297e5e2ef3d82e232454018403f4bdeecae
mfaye1 <mamadoufaye2002@gmail.com>
2016-11-17 14:54:02 -0500

    la page catalogue

--- script/catalogue.js
+++ script/catalogue.js
@@ -6,37 +6,64 @@
 $(function(){
     // Au chargement de la page, on crée le catalogue (liste des forfaits)
 
-    for (var i = 0 ; i <= categories_p86.forfaits.length - 1;  i++) {
+    for (var i = 0 ; i < P86_TP_forfaits.length;  i++) {
 
-        var forfait = categories_p86.forfaits[i]; // Le forfait considéré
-        console.log(forfait);
+        var forfait = P86_TP_forfaits[i]; // Le forfait considéré
+        console.log('Forfait : ' , forfait.nom);
 
         // Créer et accrocher un nouveau li
+        var ul_du_forfait = $('ul#' + forfait.categorie);
+        console.log('Le ul ou le forfait va être accroché : ', ul_du_forfait);
 
         var nouveau_li = $('<li>')
-            .appendTo($('#categories_p86.categories').children('ul.forfaits'))
+            .appendTo(ul_du_forfait)
             .addClass('li_du_catalogue');
         // Créer et accrocher un nouveau h2
         $('<h2>')
             .appendTo(nouveau_li)
-            .text(forfait.name);
+            .text(forfait.nom)
+            .css({
+                'text_align': 'center'
+            });
         // Créer et accrocher un nouveau img
         $('<img>')
             .appendTo(nouveau_li)
-            .prop('src', 'images/' + 'hiver/'+1+'.jpg')
-            .prop('alt', 'hiver');
-        $('<p>')
-            .appendTo(new_li)
+            .prop('src', 'images/' + forfait.photo)
+            .prop('alt', 'hiver')
+            .css({
+                display: 'inline-block',
+                width : '200px',
+                height : '250px',
+                'text-align': 'center',
+                border :'solid 2px darkgray',
+                'border-radius': '10px'
+            });
+        /*$('<p>')
+            .addClass('para')
+            .appendTo(nouveau_li)
             .html(forfait.description);
+            */
         $('<a href="#">')
-            .appendTo(new_li)
+            .appendTo(nouveau_li)
             .addClass('forfait_detail')
-            .text('Détails');
-        $('<ul>')
-            .appendTo(new_li)
+            .text('Détails')
+            .css({
+                'border-radius': '6px',
+                cursor: 'pointer',
+                height: '20px',
+                display: 'block',
+                width: '75px',
+                'text-align': 'center'
+            });
+        /*$('<ul>')
+            .addClass('details')
+            .appendTo(nouveau_li)
             .append('<li>'+forfait.prix_moy_saison+'</li>')
             .append('<li>'+forfait.categorie+'</li>')
             .append('<li>'+forfait.duree+'</li>')
             .append('<li>'+forfait.debut_saison+'</li>')
             .append('<li>'+forfait.fin_saison+'</li>')
+            */
     }
+});
+
