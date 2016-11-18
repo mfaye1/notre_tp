97dbf3f18eb30f79f78cb0eb53e9fd68a3dc1f7d
mfaye1 <mamadoufaye2002@gmail.com>
2016-11-18 14:14:48 -0500

    commit a 14h 15

--- script/catalogue.js
+++ script/catalogue.js
@@ -53,7 +53,7 @@ $(function(){
             });
 
     }
-
+/*----------------------------------------------------------------------------------------------------------------------*/
     $('.mb_background').on('click', function(){
         $(this)
             .parent()
@@ -68,72 +68,61 @@ $(function(){
         // L.attribut href du a est au format forfait_xx
         var forfait_id = $(this).attr("href").split('_')[1];
         console.log(forfait_id);
-
         forfait = P86_TP_forfaits[forfait_id];
         console.log(forfait);
 
-
         //montrer tout les forfait faite ce vendredi
 
-/*
         var modal=$('#reserver');
         console.log(modal);
-
-
-
-//creer d'abord les titres a afficher
-        var nouveau_h=$('<h3>')
-            .appendTo(modal)
-            .text('Ce Forfait est composé');
-
-
-//ajouter un ul
-        var nouveau_ul_modal_box=$('ul#' + forfait.categorie);
-
-//ajouter li
-        var li_modal=$('<li>')
-            .appendTo(nouveau_ul_modal_box);
-        $('<h2>')
-            .appendTo(nouveau_ul_modal_box)
-            .text(forfait.nom)
-            .css({
-                'text_align': 'center'
-            });
-        // Créer et accrocher un nouveau img
-        $('<img>')
-            .appendTo(nouveau_ul_modal_box)
-            .prop('src', 'images/' + forfait.photo)
-            .prop('alt', 'hiver')
-            .css({
-                float : 'left',
-                display: 'inline-block',
-                width : '200px',
-                height : '250px',
-                'text-align': 'center',
-                border :'solid 2px darkgray',
-                'border-radius': '10px'
-            });
-        $('<button><a href="#"></a></button>')
-            .appendTo(nouveau_ul_modal_box)
-            .addClass('forfait_detail')
-            .text('Détails')
-            .css({
-                'border-radius': '6px',
-                cursor: 'pointer',
-                height: '20px',
-                display: 'block',
-                width: '75px',
-                'text-align': 'center'
-            });
-
-
-//creer le boutton de la reservation
-
-        var control=$('.control');
-        $('<button><a href="formulaire.html"></a></button>')
-            .appendTo(control)
-            .text('Reserver');
-
+            var ul_du_box = $('#box');
+            var li_du_box=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Nom :  '+forfait.nom);
+            var li_du_box_2=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Categorie  :  '+forfait.categorie);
+            var li_du_box_3=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Description  :  '+forfait.description);
+            var li_du_box_4=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Duree  :  '+forfait.duree);
+            var li_du_box_5=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Debut Saison  :  '+forfait.debut_saison);
+            var li_du_box_6=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Fin Saison  :  '+forfait.fin_saison);
+            var li_du_box_7=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Mois Basse Saison  :  '+forfait.mois_basse_saison);
+            var li_du_box_8=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Prix Basse Saison : '+forfait.prix_basse_saison);
+            var li_du_box_9=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Mois Moyen Saison : '+forfait.mois_moy_saison);
+            var li_du_box_10=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Prix Moy Saison :  '+forfait.prix_moy_saison);
+            var li_du_box_11=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Mois Haute Saison : ' +forfait.mois_haute_saison);
+            var li_du_box_12=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Prix Haute Saison : '+forfait.prix_haute_saison);
+            var li_du_box_13=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Place Disponoble :  '+forfait.places_dispo);
+            var li_du_box_14=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Nombre Maximum D\'animal : '+forfait.nbr_max_animaux_admis);
+            var li_du_box_15=$('<li>')
+                    .appendTo(ul_du_box)
+                    .text('Prix Animal : '+forfait.prix_animal);
+            var le_reserver= $('<button><a href="formulaire.html">Resever</a></button>')
+                    .appendTo(ul_du_box);
 
         // Assurer la fermeture sur le click sur les deux hyperliens du div.controls
         modal
@@ -151,7 +140,7 @@ $(function(){
             .show()
             .parent()
             .fadeIn();
-*/
+
     });
 
 });
